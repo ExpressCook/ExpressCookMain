bf80fbcfb05d6b71a8955844fce57cbc795add64
SugandhaSangal <sugandhasangal@gmail.com>
2014-11-24 00:10:02 +0000

    Vision problems rectified

--- Vision/vision.cpp
+++ Vision/vision.cpp
@@ -13,8 +13,8 @@ Vision::Vision()
 void Vision::imgCapture(int condition)
 {
     VideoCapture capture(0);
-    capture.set(CV_CAP_PROP_FRAME_WIDTH,120);
-    capture.set(CV_CAP_PROP_FRAME_HEIGHT,160);
+    capture.set(CV_CAP_PROP_FRAME_WIDTH,1280);
+    capture.set(CV_CAP_PROP_FRAME_HEIGHT,720);
     if(!capture.isOpened())
     {
         cout << "Failed to connect to the camera." << endl;
@@ -27,6 +27,8 @@ void Vision::imgCapture(int condition)
         {
         cout << "Failed to capture an image" << endl;
         }
+        else
+            imwrite("Original.jpg",img);
     }
     else
     {
@@ -36,6 +38,8 @@ void Vision::imgCapture(int condition)
         {
         cout << "Failed to capture an image" << endl;
         }
+        else
+            imwrite("New.jpg",imgNew);
     }
 
 
@@ -46,14 +50,18 @@ void Vision::compute()
     vector <Point2f> srcPoints;
     vector <Point2f> dstPoints;
 
-    srcPoints.push_back(Point2f(48,20));
+    /*srcPoints.push_back(Point2f(48,20));
     srcPoints.push_back(Point2f(119,17));
     srcPoints.push_back(Point2f(53,106));
-    srcPoints.push_back(Point2f(118,104));
+    srcPoints.push_back(Point2f(118,104));*/
+    srcPoints.push_back(Point2f(394,50));
+    srcPoints.push_back(Point2f(953,12));
+    srcPoints.push_back(Point2f(436,706));
+    srcPoints.push_back(Point2f(950,699));
     dstPoints.push_back(Point2f(1,1));
-    dstPoints.push_back(Point2f(120,1));
-    dstPoints.push_back(Point2f(1,160));
-    dstPoints.push_back(Point2f(120,160));
+    dstPoints.push_back(Point2f(640,1));
+    dstPoints.push_back(Point2f(1,640));
+    dstPoints.push_back(Point2f(640,640));
 
     Mat H;
     //Mat img_out = Mat::zeros( img.size(), CV_8UC3 );
@@ -62,29 +70,34 @@ void Vision::compute()
     //imwrite("warpedoriginal.jpg",img_out);
     //Mat imgNew;
     //imgNew=imread("/home/ubuntu/vision/openCV_Learning/Learning3/PR4/New.jpg",1);
-    Mat imgNew_out = Mat::zeros( imgNew.size(), CV_8UC3 );
+    cout<<"Size of the image "<<imgNew.size()<<endl;
+    Mat imgNew_out = Mat::zeros( 640, 640, CV_8UC3 );
     //cout<<"Sugandha printing new image"<<endl;
     //imwrite("New.jpg",imgNew);
-    warpPerspective(imgNew, imgNew_out, H, imgNew.size(), 1, 1);
-    Mat img_out = Mat::zeros( img.size(), CV_8UC3 );
-    warpPerspective(img, img_out, H, img.size(), 1, 1);
+    warpPerspective(imgNew, imgNew_out, H, imgNew_out.size(), 1, 1);
+    Mat img_out = Mat::zeros( 640, 640, CV_8UC3 );
+    warpPerspective(img, img_out, H, img_out.size(), 1, 1);
     //imwrite("WarpedNew.jpg", imgNew_out);
     //Mat img_out=imread("warpedOriginal.jpg",1);
     Mat imgChange;
     absdiff(imgNew_out,img_out,imgChange);
-    //imwrite("BackgroundChange.jpg",imgChange);
+    //imshow("Changed Image",imgChange);
+    imwrite("BackgroundChange.jpg",imgChange);
     Mat imgGray;
     cvtColor(imgChange, imgGray, COLOR_BGR2GRAY);
-    //imwrite("GrayScaleImage.jpg",imgGray);
+    //imshow("Gray Scale Image",imgGray);
+    imwrite("GrayScaleImage.jpg",imgGray);
     Mat imgBw;
     //Otsu thresholding operation
     //threshold( imgGray, imgBw, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU );
     threshold( imgGray, imgBw, 10, 255,cv::THRESH_BINARY);
-    //imwrite("BWimage.jpg", imgBw);
+    //imshow("BW image",imgBw);
+    imwrite("BWimage.jpg", imgBw);
    // Mat imgDilute;
    // dilate(imgBw,imgDilute, getStructuringElement(MORPH_ELLIPSE, Size(3,3), Point(-1,-1)));
     Mat imgErode;
     erode(imgBw,imgErode, getStructuringElement(cv::MORPH_ELLIPSE, Size(3,3), Point(-1,-1)));
+    cout<<"Size of eroded image is "<<imgErode.size()<<endl;
     //imwrite("Erodedimage.jpg", imgErode);
     Mat dst = Mat::zeros(imgErode.rows, imgErode.cols, CV_8UC3);
     vector<vector<Point> > contours;
@@ -109,12 +122,12 @@ void Vision::compute()
     for(int i=0;i<num;i++)
     {
     areas.push_back(contourArea(contours[i]));
-    // cout << "Contour area is" << contourArea(contours[i])<<endl ;
+    cout << i<<" Contour area is" << contourArea(contours[i])<<endl ;
     }
    Mat1d size_mat(1, areas.size());
    memcpy(size_mat.data,areas.data(),areas.size()*sizeof(float));
    cv::sortIdx(size_mat, ind, cv::SORT_EVERY_ROW | cv::SORT_DESCENDING);
-   cout<<"Biggest bounding rectangeles"<<ind(0,1)<<endl<<ind(0,2)<<endl;
+   cout<<"Biggest bounding rectangeles"<<ind(0,0)<<endl<<ind(0,1)<<endl;
     //namedWindow( "Components", 1 );
    //imwrite( "Components.jpg", dst );
    cout<<"Bounding Rectangles"<<endl;
@@ -153,8 +166,9 @@ void Vision::compute()
 CentrePoint Vision::AppleCentroid()
 {
     CentrePoint c;
-    c.x=(89-centroids.at(ind(0,2)).y)*(25/3);
-    c.y=(centroids.at(ind(0,2)).x-46)*7.3;
+    cout<<"x :"<<centroids.at(ind(0,1)).x<<endl<<"y :"<<centroids.at(ind(0,1)).y<<endl;
+    c.x=((430/640)*640-centroids.at(ind(0,1)).x)*(500/(430-185));
+    c.y=(centroids.at(ind(0,1)).y-194)*(720/(481-194));
     return c;
     //cv::Point2f appleCentroid;
     //cv::Point2f appleCentroid((89-centroids.at(ind(0,2)).y)*(25/3), (centroids.at(ind(0,2)).x-46)*7.3);
@@ -167,8 +181,11 @@ CentrePoint Vision::AppleCentroid()
 CentrePoint Vision::PotatoCentroid()
 {
     CentrePoint c;
-    c.x=(89-centroids.at(ind(0,1)).y)*(25/3);
-    c.y=(centroids.at(ind(0,1)).x-46)*7.3;
+    cout<<"x :"<<centroids.at(ind(0,0)).x<<endl<<"y :"<<centroids.at(ind(0,0)).y<<endl;
+    c.x=(430-centroids.at(ind(0,0)).x)*(2.04);
+    c.y=(centroids.at(ind(0,0)).y-194)*2.51;
+    cout<<"Centroid Points are x:"<<c.x<<endl;
+    cout<<"Centroid Points are y:"<<c.y<<endl;
     return c;
     //cv::Point2f potatoCentroid;
     //cv::Point2f potatoCentroid((89-centroids.at(ind(0,1)).y)*(25/3), (centroids.at(ind(0,1)).x-46)*7.3);
